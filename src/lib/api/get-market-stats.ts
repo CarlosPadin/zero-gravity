341c69b193e855c5bb1310b89dc0c95d19eabac8
Carlos Padin <padin790@gmail.com>
2026-01-21 12:01:05 -0500

    Security fix

--- src/lib/api/get-market-stats.ts
+++ src/lib/api/get-market-stats.ts
@@ -1,17 +1,19 @@
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+
 export async function getOverviewStats() {
-  const response = await fetch("http://127.0.0.1:8000/metrics/overview");
+  const response = await fetch(`${BACKEND_URL}/metrics/overview`);
   if (!response.ok) throw new Error("Failed to fetch: " + response.status);
   return response.json();
 }
 
 export async function getMarketVolumes() {
-  const response = await fetch("http://127.0.0.1:8000/market-volume/last-7-days");
+  const response = await fetch(`${BACKEND_URL}/volumes/latest`);
   if (!response.ok) throw new Error("Failed to fetch: " + response.status);
   return response.json();
 }
 
 export async function getMarketExchanges() {
-  const response = await fetch("http://127.0.0.1:8000/exchanges/latest");
+  const response = await fetch(`${BACKEND_URL}/exchanges/latest`);
   console.log('response: ', response)
   if (!response.ok) throw new Error("Failed to fetch: " + response.status);
   return response.json();
